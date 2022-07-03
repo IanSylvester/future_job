37cd0e1659f69baef18f90885d2667082684675b
IanSylvester <sylvester16@gmail.com>
2022-07-03 17:43:12 -0400

    added subCategory route, also added a get all by sub and by vendor in product route

--- api/routes/productRoutes.js
+++ api/routes/productRoutes.js
@@ -22,6 +22,28 @@ router.get("/:category_id", async (req, res) => {
   }
 });
 
+//get all products by subcategory
+router.get("/subcategory/:subcategory_id", async (req, res) => {
+  const products = await Product.find({ subcategory_id: req.params.subcategory_id});
+  try {
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+}
+);
+
+//get all products by vendor
+router.get("/vendor/:vendor_id", async (req, res) => {
+  const products = await Product.find({ vendor_id: req.params.vendor_id});
+  try {
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+}
+);
+
 // Create Product
 router.post("/", async (req, res) => {
   const newProduct = new Product(req.body);
@@ -32,5 +54,7 @@ router.post("/", async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+
 
 module.exports = router;
---
+++ api/routes/subCategoryRoutes.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const router = express.Router();
+const SubCategory = require("../models/SubCategory");
+
+//get all subcategories
+router.get("/", async (req, res) => {
+    const subcategories = await SubCategory.find();
+    try {
+        res.status(200).json(subcategories);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+    }
+);
+
+// get all subcategories by category
+router.get("/:category_id", async (req, res) => {
+    const subcategories = await SubCategory.find({ category_id: req.params.category_id});
+    try {
+        res.status(200).json(subcategories);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+    }
+);
+
+//create subcategory
+router.post("/", async (req, res) => {
+    const newSubCategory = new SubCategory(req.body);
+    try {
+        const savedSubCategory = await newSubCategory.save();
+        res.status(200).json(savedSubCategory);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+    }
+);
+
+
